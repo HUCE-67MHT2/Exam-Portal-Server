eb077a1a5fffcceb6a5f29893f32f2a4f5942795
htung1901 <nvht2004@gmail.com>
2025-02-17 09:18:45 +0700

    feat: add HomeController to handle home route

---
+++ src/main/java/com/exam_portal_server/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.exam_portal_server.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Home";
+    }
+}
