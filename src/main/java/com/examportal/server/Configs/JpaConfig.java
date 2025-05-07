474e24bbb8d2d1f2188e76849f0578afd61657f0
ngoqh <ngoqhuy1711@gmail.com>
2025-05-07 17:24:18 +0700

    fix: update database connection URL to use the correct MySQL port

--- src/main/java/com/examportal/server/Configs/JpaConfig.java
+++ src/main/java/com/examportal/server/Configs/JpaConfig.java
@@ -34,7 +34,7 @@ public class JpaConfig {
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .url("jdbc:mysql://localhost:3307/exam_portal_management")
+                .url("jdbc:mysql://localhost:3306/exam_portal_management")
                 .username("root")
                 .password("123456")
                 .build();
